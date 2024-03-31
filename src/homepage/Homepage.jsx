849d4a336b02e68e30ca99387e16d274a5a2f85d
DebCh20 <1804020@kiit.ac.in>
2024-03-31 09:49:31 +0530

    added typewriter effect on the homepage

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -5,9 +5,10 @@ import { Link, Navigate } from "react-router-dom";
 function Homepage() {
   return (
     <div className='homepageContainer' style={{width:'100%'}}>
-        <h1 style={{color:'white', fontFamily:'Verdana', fontSize:'120px', textAlign:'center'}}>Rock. Paper. Scissor.</h1>
+        <h1 ><span className="line1">Rock. Paper.</span><br/>
+          <span className="line2">Scissor.</span></h1>
         <h3 style={{textAlign:'center',color:'white', fontFamily:'Verdana', fontWeight:'lighter'}}>The childhood game now got even more interesting !!</h3>
-        <br/>
+        
         <Link style={{textDecoration:'none', color:"white", marginLeft:'45%', fontSize:"20px"
         , fontFamily:"Verdana", border:"1px solid teal", padding:'10px', borderRadius:'5px'}} 
         to="/gamepage">Start Game</Link>
